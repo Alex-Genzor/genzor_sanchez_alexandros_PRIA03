b837193ac6527018feafddf648b38290543ebc43
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2025-02-28 14:59:16 +0100

    added funcs OnTriggerEnter2D() to allow agent to interact with environment, and OnEpisodeBegin()

--- Assets/Scripts/MoveToTargetAgent.cs
+++ Assets/Scripts/MoveToTargetAgent.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MoveToTargetAgent : Agent
 {
@@ -24,12 +26,37 @@ public class MoveToTargetAgent : Agent
 
         float movementSpeed = 5f;
         
-        transform.localPosition += new Vector3(moveX, moveY, 0) * movementSpeed * Time.deltaTime;
+        transform.localPosition += new Vector3(moveX, moveY, 0) * 
+                                   movementSpeed * Time.deltaTime;
 
     }
 
-    
-    
+    // refactor pending
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            EndEpisode();
+
+        } else if (collision.TryGetComponent(out Wall wall))
+        {
+            AddReward(-2f);
+            EndEpisode();
+
+        }
+        
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-3.5f, -1.5f), 
+            Random.Range(-3.5f, 3.5f));
+        target.localPosition = new Vector3(Random.Range(1.5f, 3.5f), 
+            Random.Range(-3.5f, 3.5f));
+        
+    }
+
     /*// Start is called before the first frame update
     void Start()
     {
